a036edba828a66d2c51f8136d62df780fb351861
implodingduck <zsek320@gmail.com>
2021-06-09 13:17:51 -0500

    removing key in policy for now

--- webappvm/main.tf
+++ webappvm/main.tf
@@ -83,19 +83,6 @@ resource "azurerm_key_vault" "kv" {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
-    key_permissions = [
-      "Get",
-      "List",
-      "Set",
-      "Delete",
-      "Purge",
-      "Create",
-      "Import",
-      "Update",
-      "Decrypt",
-      "Encrypt"
-    ]
-
     secret_permissions = [
       "Get",
       "List",

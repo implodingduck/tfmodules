0c1ea56d6ee68e88cb5dc11695f23a50fd29284c
implodingduck <zsek320@gmail.com>
2021-06-10 16:57:05 -0500

    alright lets try this

--- webappvm/main.tf
+++ webappvm/main.tf
@@ -238,7 +238,7 @@ resource "azurerm_lb" "vmss" {
 
  frontend_ip_configuration {
    name                 = "PublicIPAddress"
-   public_ip_address_id = azurerm_public_ip.vmss.id
+   public_ip_address_id = azurerm_public_ip.pip.id
  }
 
  tags = merge({managed_by = "terraform"}, var.tags)

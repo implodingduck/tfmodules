4b8e4b58600dd37c8944e42936a4ad29de3e5f1a
implodingduck <zsek320@gmail.com>
2021-06-11 14:20:07 -0500

    adding stuff to reduce run variance

--- webappvm/main.tf
+++ webappvm/main.tf
@@ -13,6 +13,7 @@ resource "random_string" "unique" {
 resource "azurerm_resource_group" "rg" {
     name = "rg-${var.name}-webapp-${local.loc_for_naming}"
     location = var.location
+    tags = merge({ managed_by = "terraform" }, var.tags)
 }
 
 resource "azurerm_storage_account" "sa" {
@@ -21,10 +22,7 @@ resource "azurerm_storage_account" "sa" {
     location                    = azurerm_resource_group.rg.location
     account_replication_type    = "LRS"
     account_tier                = "Standard"
-
-    tags = {
-        managed_by = "terraform"
-    }
+    tags                        = merge({ managed_by = "terraform" }, var.tags)
 }
 
 resource "azurerm_virtual_network" "default" {
@@ -32,7 +30,7 @@ resource "azurerm_virtual_network" "default" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = [var.vnet_cidr]
-
+  dns_servers         = []
   tags = {
     managed_by = "terraform"
   }
@@ -51,6 +49,8 @@ resource "azurerm_subnet" "vm" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.default.name
   address_prefixes     = [var.vm_subnet_cidr]
+  service_endpoint_policy_ids = []
+  service_endpoints           = []
 
 }
 
@@ -76,7 +76,11 @@ resource "azurerm_key_vault" "kv" {
       "Delete",
       "Purge"
     ]
+    certificate_permissions = []
+    key_permissions = []
+    storage_permissions = []
   }
+  tags = merge({managed_by = "terraform"}, var.tags)
 }
 
 resource "random_password" "password" {
@@ -89,6 +93,7 @@ resource "azurerm_key_vault_secret" "vmpassword" {
   name         = "vmpassword"
   value        = random_password.password.result
   key_vault_id = azurerm_key_vault.kv.id
+  tags         = merge({managed_by = "terraform"}, var.tags)
 }
 
 
@@ -98,140 +103,14 @@ resource "azurerm_public_ip" "pip" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
+  zones               = []
+  ip_tags             = {}
+  tags                = merge({managed_by = "terraform"}, var.tags)
 }
-
-# resource "azurerm_lb" "lb" {
-#   name                = "lb${var.name}"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-
-#   frontend_ip_configuration {
-#     name                 = "PublicIPAddress"
-#     public_ip_address_id = azurerm_public_ip.pip.id
-#   }
-# }
-
-# resource "azurerm_lb_backend_address_pool" "azlb" {
-#   name                = "BackEndAddressPool"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   loadbalancer_id     = azurerm_lb.lb.id
-# }
-
-# resource "azurerm_lb_nat_rule" "rule" {
-#   count                          = var.num_vms
-#   resource_group_name            = azurerm_resource_group.rg.name
-#   loadbalancer_id                = azurerm_lb.lb.id
-#   name                           = "http-${count.index}"
-#   protocol                       = "Tcp"
-#   frontend_port                  = "808${count.index}"
-#   backend_port                   = 80
-#   frontend_ip_configuration_name = "PublicIPAddress"
-# }
-
-# resource "azurerm_lb_probe" "azlb" {
-#   count               = var.num_vms
-#   name                = "probe-${var.name}-${count.index}"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   loadbalancer_id     = azurerm_lb.lb.id
-#   protocol            = "Http"
-#   port                = 80
-#   interval_in_seconds = 30
-#   number_of_probes    = 3
-#   request_path        = "/"
-# }
-
-# resource "azurerm_lb_rule" "azlb" {
-#   count                          = var.num_vms
-#   name                           = "lb-rule-${var.name}-${count.index}"
-#   resource_group_name            = azurerm_resource_group.rg.name
-#   loadbalancer_id                = azurerm_lb.lb.id
-#   protocol                       = "tcp"
-#   frontend_port                  = "808${count.index}"
-#   backend_port                   = 80
-#   frontend_ip_configuration_name = "PublicIPAddress"
-#   enable_floating_ip             = false
-#   backend_address_pool_id        = azurerm_lb_backend_address_pool.azlb.id
-#   idle_timeout_in_minutes        = 5
-#   probe_id                       = azurerm_lb_probe.azlb[count.index].id
-# }
-
-# resource "azurerm_network_interface" "nic" {
-#   count = var.num_vms
-#   name                = "${var.name}-${count.index}-nic"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   enable_ip_forwarding = true
-#   ip_configuration {
-#     name                          = "myipconfig${count.index}"
-#     subnet_id                     = azurerm_subnet.vm.id
-#     private_ip_address_allocation = "Dynamic"
-#     #load_balancer_backend_address_pools_ids = [azurerm_lb_backend_address_pool.azlb.id]
-#     #load_balancer_inbound_nat_rules_ids = [azurerm_lb_nat_rule.rule[count.index].id]
-#   }
-# }
-
-# resource "azurerm_network_interface_nat_rule_association" "assoc" {
-#   count = var.num_vms
-#   network_interface_id  = azurerm_network_interface.nic[count.index].id
-#   ip_configuration_name = "myipconfig${count.index}"
-#   nat_rule_id           = azurerm_lb_nat_rule.rule[count.index].id
-# }
-
-# resource "azurerm_availability_set" "set" {
-#   name                = "aset-${var.name}"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   managed             = true
-#   tags = {
-#         managed_by = "terraform"
-#     }
-# }
 
 data "template_file" "nginx-vm-cloud-init" {
   template = file("${path.module}/install-nginx.sh")
 }
-
-# resource "azurerm_linux_virtual_machine" "vm" {
-#     count                 = var.num_vms
-#     name                  = "${var.name}-vm-${count.index}"
-#     location              = azurerm_resource_group.rg.location
-#     resource_group_name   = azurerm_resource_group.rg.name
-#     network_interface_ids = [azurerm_network_interface.nic[count.index].id]
-#     size                  = var.vm_size
-#     availability_set_id   = azurerm_availability_set.set.id
-#     os_disk {
-#         name              = "${var.name}vm${count.index}osdesk"
-#         caching           = "ReadWrite"
-#         storage_account_type = "Premium_LRS"
-#     }
-
-#     source_image_reference {
-#         publisher = "Canonical"
-#         offer     = "0001-com-ubuntu-server-focal"
-#         sku       = "20_04-lts-gen2"
-#         version   = "latest"
-#     }
-
-#     computer_name  = "${var.name}-vm-${count.index}"
-#     admin_username = "azureuser"
-#     admin_password = random_password.password.result
-#     disable_password_authentication = false
-#     custom_data    = base64encode(data.template_file.nginx-vm-cloud-init.rendered)
-#     # admin_ssh_key {
-#     #     username       = "azureuser"
-#     #     public_key     = tls_private_key.example_ssh.public_key_openssh 
-#     # }
-
-#     boot_diagnostics {
-#         storage_account_uri = azurerm_storage_account.sa.primary_blob_endpoint
-#     }
-
-#     tags = {
-#         managed_by = "terraform"
-#     }
-# }
-
-
 
 resource "azurerm_lb" "vmss" {
  name                = "${var.name}-lb"
@@ -239,8 +118,10 @@ resource "azurerm_lb" "vmss" {
  resource_group_name = azurerm_resource_group.rg.name
 
  frontend_ip_configuration {
-   name                 = "PublicIPAddress"
-   public_ip_address_id = azurerm_public_ip.pip.id
+   name                       = "PublicIPAddress"
+   public_ip_address_id       = azurerm_public_ip.pip.id
+   private_ip_address_version = "IPv4"
+   zones                      = []
  }
 
  tags = merge({managed_by = "terraform"}, var.tags)
@@ -294,6 +175,8 @@ resource "azurerm_virtual_machine_scale_set" "vmss" {
    caching           = "ReadWrite"
    create_option     = "FromImage"
    managed_disk_type = "Standard_LRS"
+   os_type           = "Linux"
+   vhd_containers    = []
  }
 
  storage_profile_data_disk {

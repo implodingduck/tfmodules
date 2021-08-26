7d9202ca4e8399aa49d7211d0141d8214de928d1
implodingduck <zsek320@gmail.com>
2021-08-26 15:53:04 -0500

    adding possible outbound ip address list

--- appservice/outputs.tf
+++ appservice/outputs.tf
@@ -1,3 +1,7 @@
 output "app_service_name" {
   value = azurerm_app_service.as.name
 }
+
+output "possible_outbound_ip_address_list" {
+  value = azurerm_app_servoce.as.possible_outbound_ip_address_list
+}

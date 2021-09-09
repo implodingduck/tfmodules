08734c7ab7345dd3e6dfc10e4d2f9d9556c2c209
implodingduck <zsek320@gmail.com>
2021-09-08 19:47:01 -0500

    exposing the indentity

--- functionapp/outputs.tf
+++ functionapp/outputs.tf
@@ -1,3 +1,11 @@
 output "possible_outbound_ip_addresses" {
     value = split("," ,azurerm_function_app.func.possible_outbound_ip_addresses)
 }
+
+output "identity_principal_id" {
+  value = azurerm_function_app.func.identity.0.principal_id
+}
+
+output "identity_tenant_id" {
+  value = azurerm_function_app.func.identity.0.tenant_id
+}

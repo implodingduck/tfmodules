2a76b92da50ff613307d6e20b124253bb2f595fe
implodingduck <zsek320@gmail.com>
2021-09-17 10:54:57 -0500

    next attempt

--- functionapp/outputs.tf
+++ functionapp/outputs.tf
@@ -3,11 +3,11 @@ output "possible_outbound_ip_addresses" {
 }
 
 output "identity_principal_id" {
-  value = lookup(azurerm_function_app.func.identity, "0", { principal_id=null }).principal_id
+  value = lookup(azurerm_function_app.func.identity.0, "principal_id", null )
 }
 
 output "identity_tenant_id" {
-  value = lookup(azurerm_function_app.func.identity, "0", { tenant_id=null }).tenant_id
+  value = lookup(azurerm_function_app.func.identity.0, "tenant_id", null)
 }
 
 output "function_id" {

4510ca16733a64e29411166bb6786eb7c9871223
implodingduck <zsek320@gmail.com>
2022-01-18 12:10:42 -0600

    adding in more outputs

--- sql/outputs.tf
+++ sql/outputs.tf
@@ -1,3 +1,12 @@
 output db_server_id {
     value = azurerm_mssql_server.db.id
 }
+
+output db_fully_qualified_domain_name {
+    value = azurerm_mssql_server.db.fully_qualified_domain_name
+}
+
+output db_name {
+    value = azurerm_mssql_database.db.name
+}
+
